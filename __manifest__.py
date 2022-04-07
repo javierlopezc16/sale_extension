b2ef98ca54a1d4709392febe05e2d7523040bdec
Javier Lopez <javierlopezc16@gmail.com>
2022-04-06 23:16:38 -0400

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+
+{
+    "name": "SO Extensions",
+    "version": "15.0.1.0.0",
+    "author": "Consultor sin nombre",
+    "maintainer": "Consultor sin nombre",
+    "website": "http://consultorsinnombre.com",
+    "summary": "Extensiones en el modulo sale",
+    "support": "javierlopezc16@gmail.com",
+    "category": "Sales",
+    "depends": ["sale"],
+    "data": [
+        # Views
+        "views/sale_order.xml"
+    ],
+    "installable": True,
+    "application": True,
+    "license": 'LGPL-3'
+}
